ecc84a17a2a9082ef12f0426396d2af057c29f16
oudarja <oudarja360@gmail.com>
2025-10-25 22:05:22 +0600

    all extension done and fixed

--- testing_ml_api.py
+++ testing_ml_api.py
@@ -1,1 +1,33 @@
 # here the dockerized API is being tested
+# Here dockerized api will be consumed
+import requests
+import json
+
+url = "http://localhost:8000/predict"
+
+sample_data = {
+        "CRIM": 0.1,
+        "ZN": 18.0,
+        "INDUS": 2.31,
+        "CHAS": 0,
+        "NOX": 0.538,
+        "RM": 6.575,
+        "AGE": 65.2,
+        "DIS": 4.09,
+        "RAD": 1,
+        "TAX": 296.0,
+        "PTRATIO": 15.3,
+        "B": 396.9,
+        "LSTAT": 4.98
+    }
+
+
+json_data = json.dumps(sample_data)
+
+response = requests.post(url, data=json_data)
+
+prediction = response.json().get("predicted_price")
+
+print(f"Predicted house price: {prediction}")
+
+# Predicted house price: 29.942805298376744
